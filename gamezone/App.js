bdd30583aabf862870245d962c2c521377f817e5
ucwealth <ucheokeke16@gmail.com>
2022-03-29 20:57:05 +0100

    create game review app

---
+++ gamezone/App.js
@@ -0,0 +1,11 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import Home from './src/screens/home'
+
+export default function App() {
+  return (
+    <Home />
+  )
+}
+
+const styles = StyleSheet.create({})
