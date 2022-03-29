2860d0d28946249b62741a07cc5b1a7951c22d37
ucwealth <ucheokeke16@gmail.com>
2022-03-29 22:50:44 +0100

    create navigation stack

---
+++ gamezone/src/styles/global.js
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native'
+
+export const globalStyles = StyleSheet.create({
+    container: {
+        padding: 30,
+        flex: 1,
+    },
+    titleText: {
+        fontFamily: 'Nunito-Bold',
+        fontSize: 18,
+        color: '#333'
+    },
+    paragraph: {
+        marginVertical: 10,
+        lineHeight: 20,
+    }
+})
