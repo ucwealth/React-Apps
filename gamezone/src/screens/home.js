ea75d81a9d862d8d85929271fe5d527039e2d54c
ucwealth <ucheokeke16@gmail.com>
2022-03-30 15:34:36 +0100

    navigate to detail screen

--- gamezone/src/screens/home.js
+++ gamezone/src/screens/home.js
@@ -1,11 +1,25 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Button, FlatList, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { globalStyles } from '../styles/global'
 
-export default function Home() {
+export default function Home({ navigation }) {
+    const [reviews, setReviews] = useState([
+      { title: 'Zelda, Breath of Fresh Air', rating: 5, body: 'lorem ipsum', key: '1' },
+      { title: 'Gotta Catch Them All (again)', rating: 4, body: 'lorem ipsum', key: '2' },
+      { title: 'Not So "Final" Fantasy', rating: 3, body: 'lorem ipsum', key: '3' },
+    ])
+
   return (
     <View style={globalStyles.container}>
-      <Text style={globalStyles.titleText}>Home</Text>
+       <FlatList 
+       data={reviews}
+       renderItem={({ item }) => (
+        <TouchableOpacity onPress={() => navigation.navigate('ReviewDetails', { item }) }>
+          <Text style={globalStyles.titleText}>{item.title}</Text>
+        </TouchableOpacity>
+
+       )}
+       />
     </View>
   )
 }
--- gamezone/src/screens/reviewDetails.js
+++ gamezone/src/screens/reviewDetails.js
@@ -2,10 +2,15 @@ import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { globalStyles } from '../styles/global'
 
-export default function ReviewDetails() {
+export default function ReviewDetails({ route, navigation }) {
+  const { item } = route.params;
+
   return (
     <View style={globalStyles.container}>
-      <Text>ReviewDetails</Text>
+      <Text>{ item.title }</Text>
+      <Text>{ JSON.stringify(item.body) }</Text>
+      <Text>{ item.rating }</Text>
+
     </View>
   )
 }
