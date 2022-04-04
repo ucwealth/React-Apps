71f93a6c6ea61f346220515ed599c859eb4570ed
ucwealth <ucheokeke16@gmail.com>
2022-04-04 14:55:02 +0100

    debug rnvectoricons error

--- gamezone/react-native.config.js
+++ gamezone/react-native.config.js
@@ -1,7 +1,14 @@
 module.exports = {
-    project: {
-        ios:{},
-        android:{}
-    },
-    assets:['./src/assets/fonts/'],
+  project: {
+    ios:{},
+    android:{}
+  },
+  dependencies: {
+      'react-native-vector-icons': {
+        platforms: {
+          ios: null,
+        },
+      },
+  },
+  assets:['./src/assets/fonts/'],
 }
