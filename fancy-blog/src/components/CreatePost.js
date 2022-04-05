ae7be414c1570371b0aabae0cc1dc8d22be8cfe3
ucwealth <ucheokeke16@gmail.com>
2022-04-05 12:39:56 +0100

    create post form

--- fancy-blog/src/components/CreatePost.js
+++ fancy-blog/src/components/CreatePost.js
@@ -1,7 +1,26 @@
  const CreatePost = () => {
     return ( 
-        <div className="create-post">
+        <div className="create">
             <h1>Add New Post</h1>
+            <form action="" method="post">
+                <label>Post title:</label>
+                <input 
+                type="text" 
+                required
+                />
+                <label>Post body: </label>
+                <textarea required>
+                    
+                </textarea>
+                <label>Post author: </label>
+                <select name="" id="">
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                    <option value="wiki">wiki</option>
+                    <option value="only fans">only fans</option>
+                </select>
+                <button type="submit">Add Post</button>
+            </form>
         </div>
      );
 }
