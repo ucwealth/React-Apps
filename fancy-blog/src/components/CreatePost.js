54d987dc95b89e6a1ae3ac4cf0a5b24e2c5e8c5f
ucwealth <ucheokeke16@gmail.com>
2022-04-07 10:20:08 +0100

    send post to json server

--- fancy-blog/src/components/CreatePost.js
+++ fancy-blog/src/components/CreatePost.js
@@ -4,17 +4,22 @@ import { useState } from "react";
      const [title, setTitle] = useState('')
      const [body, setBody] = useState('')
      const [author, setAuthor] = useState('mario')
+     const [isPending, setIsPending] = useState(false)
 
      const handleSubmit = (e) => {
          e.preventDefault()
          const post = {title, body, author}
+
+         setIsPending(true)
+
          fetch('http://localhost:8000/posts', {
              method: 'POST',
              headers: { "Content-Type": "application/json" },
              body: JSON.stringify(post)
-         }).then(
+         }).then(() => {
              console.log('new post added')
-         )
+             setIsPending(false)
+         })
      }
      
     return ( 
@@ -42,7 +47,8 @@ import { useState } from "react";
                     <option value="wiki">wiki</option>
                     <option value="only fans">only fans</option>
                 </select>
-                <button type="submit">Add Post</button>
+                { !isPending && <button type="submit">Add Post</button> }
+                { isPending && <button disabled type="submit">Adding Post...</button> }
             </form>
         </div>
      );
--- fancy-blog/src/components/Navbar.js
+++ fancy-blog/src/components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
         <h1>The Fancy Blog</h1>
         <div className="links">
             <Link to='/'>Home</Link>
-            <Link to='/create'>New Blog</Link>
+            <Link to='/create'>New Post</Link>
         </div>
     </nav>
     )
