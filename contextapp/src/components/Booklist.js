817b355964d36d0d4ce90821fa278a2e053cdc15
ucwealth <ucheokeke16@gmail.com>
2022-04-07 13:35:12 +0100

    create navbar abd booklist comps

---
+++ contextapp/src/components/Booklist.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Booklist = () => {
+    return (
+        <div className="book-list">
+            <ul>
+                <li>The Son of the House</li>
+                <li>Things fall apart</li>
+                <li>No Longer at Ease</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Booklist
---
+++ contextapp/src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <h1>Context App</h1>
+            <ul>
+                <li>Home</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </nav>
+    )
+}
+export default Navbar
