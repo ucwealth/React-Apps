b1f9632bce53f9e374bc0a8eca73c6fa98a6ee42
ucwealth <ucheokeke16@gmail.com>
2022-04-07 10:33:00 +0100

    redirect to home after posting

--- fancy-blog/src/components/CreatePost.js
+++ fancy-blog/src/components/CreatePost.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom'
 
  const CreatePost = () => {
      const [title, setTitle] = useState('')
      const [body, setBody] = useState('')
      const [author, setAuthor] = useState('mario')
      const [isPending, setIsPending] = useState(false)
+     const navigate = useNavigate()
 
      const handleSubmit = (e) => {
          e.preventDefault()
@@ -17,8 +19,9 @@ import { useState } from "react";
              headers: { "Content-Type": "application/json" },
              body: JSON.stringify(post)
          }).then(() => {
-             console.log('new post added')
              setIsPending(false)
+             console.log('new post added')
+             navigate('/')
          })
      }
      
