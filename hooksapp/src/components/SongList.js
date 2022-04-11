1480a95b5f248d7b08354a6024ece1337a890e2b
ucwealth <ucheokeke16@gmail.com>
2022-04-11 11:30:36 +0100

    add song

--- hooksapp/src/components/SongList.js
+++ hooksapp/src/components/SongList.js
@@ -1,13 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 const SongList = () => {
+    const [songs, setSongs ] = useState([
+        {title: "Beautiful Girls", id: 1},
+        {title: "Tears on my guitar", id: 2},
+        {title: "Welcome to Paradise", id: 3},
+    ])
+    const addSong = () => {
+        setSongs([
+            ...songs,
+            {title: "A new song", id: uuidv4() }
+        ])
+    }
+
   return (
     <div className="song-list">
         <ul>
-            <li>Beautiful Girls</li>
-            <li>Tears on my guitar</li>
-            <li>Welcome to Paradise</li>
+            { songs.map(song => {
+                return (
+                    <li key={song.id}>{song.title}</li>
+                )
+            }) }
         </ul>
+        <button onClick={addSong}>Add a song</button>
     </div>
   )
 }
