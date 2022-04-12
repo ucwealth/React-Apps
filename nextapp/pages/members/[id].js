23afbd79d8c17b4ae0eec8c2d352b712e3a131b6
ucwealth <ucheokeke16@gmail.com>
2022-04-12 14:58:08 +0100

    display each member's details

--- nextapp/pages/members/[id].js
+++ nextapp/pages/members/[id].js
@@ -14,10 +14,22 @@ export const getStaticPaths = async () => {
     }
 }
 
-const Details = () => {
+export const getStaticProps = async (context) => {
+    const id = context.params.id
+    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id)
+    const data = await res.json()
+    return {
+        props: { member: data }
+    }
+}
+
+const Details = ({ member }) => {
     return ( 
         <div>
-            <h1>Details</h1>
+            <h1>{member.name}</h1>
+            <p>{member.email}</p>
+            <p>{member.website}</p>
+            <p>{member.address.city}</p>
         </div>
      );
 }
