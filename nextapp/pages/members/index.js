09c049b93288d20d10db1ed8e66ce1074e7b4fd0
ucwealth <ucheokeke16@gmail.com>
2022-04-12 14:16:09 +0100

    use getstaticprops to fetch member list

--- nextapp/pages/members/index.js
+++ nextapp/pages/members/index.js
@@ -1,7 +1,19 @@
-const Members = () => {
+export const getStaticProps = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+    return {
+        props: { members: data }
+    }
+}
+const Members = ({ members }) => {
     return ( 
         <div>
             <h1>All Members</h1>
+            {members.map(member => (
+                <a>
+                    <h3>{ member.name }</h3>
+                </a>
+            ))}
         </div>
      );
 }
