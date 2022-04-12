37efa276ac759066b4aecd679c0b477d1eda95fb
ucwealth <ucheokeke16@gmail.com>
2022-04-12 14:35:20 +0100

    add dynamic routes for id

--- nextapp/pages/members/index.js
+++ nextapp/pages/members/index.js
@@ -1,4 +1,6 @@
 import styles from '../../styles/Members.module.css'
+import Link from 'next/link'
+
 export const getStaticProps = async () => {
     const res = await fetch('https://jsonplaceholder.typicode.com/users')
     const data = await res.json()
@@ -11,9 +13,11 @@ const Members = ({ members }) => {
         <div>
             <h1>All Members</h1>
             {members.map(member => (
-                <a className={styles.single}>
-                    <h3>{ member.name }</h3>
-                </a>
+                <Link href={`/members/${member.id}`} key={member.id}>
+                    <a className={styles.single}>
+                        <h3>{ member.name }</h3>
+                    </a>
+                </Link>
             ))}
         </div>
      );
