7bdf3c437eadbe48cb02e75fd18bb534c1586c35
ucwealth <ucheokeke16@gmail.com>
2022-04-12 14:50:44 +0100

    get array of ids for next SSG

--- nextapp/pages/members/[id].js
+++ nextapp/pages/members/[id].js
@@ -1,3 +1,19 @@
+export const getStaticPaths = async () => {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    const data = await res.json()
+
+    const paths = data.map(member => {
+        return {
+            params: { id: member.id.toString() }
+        }
+    })
+
+    return {
+        paths,
+        fallback: false 
+    }
+}
+
 const Details = () => {
     return ( 
         <div>
