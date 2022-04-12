9e23440870f398ca39e3866047b733971f74cce4
ucwealth <ucheokeke16@gmail.com>
2022-04-12 12:14:00 +0100

    add head and metadata

--- nextapp/pages/about.js
+++ nextapp/pages/about.js
@@ -1,10 +1,18 @@
+import Head from 'next/head'
+
 const About = () => {
     return ( 
+        <>
+        <Head>
+          <title>Member List | About </title>
+          <meta name="keywords" content="members" />
+        </Head>
         <div>
             <h1>About Us</h1>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cum temporibus accusamus provident quod corrupti dolore earum eos veritatis eius, fuga consequuntur, quisquam ipsum. Accusamus nisi quod cum voluptatibus culpa.</p>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cum temporibus accusamus provident quod corrupti dolore earum eos veritatis eius, fuga consequuntur, quisquam ipsum. Accusamus nisi quod cum voluptatibus culpa.</p>
         </div>
+        </>
      );
 }
  
--- nextapp/pages/index.js
+++ nextapp/pages/index.js
@@ -1,15 +1,22 @@
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
+import Head from 'next/head'
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.title}>HomePage</h1>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cum ipsam quod doloremque maiores vel recusandae earum asperiores sequi velit esse impedit veniam aut, aliquam cupiditate dolores consequatur numquam fugit.</p>
-      <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cum ipsam quod doloremque maiores vel recusandae earum asperiores sequi velit esse impedit veniam aut, aliquam cupiditate dolores consequatur numquam fugit.</p>
-      <Link href="/members">
-        <a className={styles.btn}>See Member Listing</a>
-        </Link>
-    </div>
+    <>
+      <Head>
+        <title>Member List | Home </title>
+        <meta name="keywords" content="members" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>HomePage</h1>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cum ipsam quod doloremque maiores vel recusandae earum asperiores sequi velit esse impedit veniam aut, aliquam cupiditate dolores consequatur numquam fugit.</p>
+        <p className={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet cum ipsam quod doloremque maiores vel recusandae earum asperiores sequi velit esse impedit veniam aut, aliquam cupiditate dolores consequatur numquam fugit.</p>
+        <Link href="/members">
+          <a className={styles.btn}>See Member Listing</a>
+          </Link>
+      </div>
+    </>
   )
 }
