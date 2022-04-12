627d1f6f045613977efaf68114d63e827a393156
ucwealth <ucheokeke16@gmail.com>
2022-04-12 10:33:44 +0100

    create next app

---
+++ nextapp/pages/about.js
@@ -0,0 +1,11 @@
+const About = () => {
+    return ( 
+        <div>
+            <h1>ABOUT PAGE</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cum temporibus accusamus provident quod corrupti dolore earum eos veritatis eius, fuga consequuntur, quisquam ipsum. Accusamus nisi quod cum voluptatibus culpa.</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi cum temporibus accusamus provident quod corrupti dolore earum eos veritatis eius, fuga consequuntur, quisquam ipsum. Accusamus nisi quod cum voluptatibus culpa.</p>
+        </div>
+     );
+}
+ 
+export default About;
