4c418c1cfaf24ee51067441df8f1466b7bb2336e
ucwealth <ucheokeke16@gmail.com>
2022-04-12 10:44:27 +0100

    add other components

---
+++ nextapp/components/Navbar.js
@@ -0,0 +1,15 @@
+
+const Navbar = () => {
+    return ( 
+        <nav>
+            <div className="logo">
+                <h1>Member List</h1>
+            </div>
+            <a>Home</a>
+            <a>About</a>
+            <a>Member Listing</a>
+        </nav>
+     );
+}
+ 
+export default Navbar;
---
+++ nextapp/pages/members/index.js
@@ -0,0 +1,9 @@
+const Members = () => {
+    return ( 
+        <div>
+            <h1>All Members</h1>
+        </div>
+     );
+}
+ 
+export default Members;
