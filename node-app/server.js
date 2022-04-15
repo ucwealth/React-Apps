c2652c0594b27e565a2c19c3e711eebbd6a220d4
ucwealth <ucheokeke16@gmail.com>
2022-04-15 23:37:58 +0100

    send html file as response

--- node-app/server.js
+++ node-app/server.js
@@ -1,14 +1,30 @@
 const http = require('http')
+const fs = require('fs')
 const port = 3000
 const server = http.createServer((req, res) => {
     console.log(req.url, req.method)
 
     // set header content type
+
     // res.setHeader('Content-Type', 'text/plain')
     // res.write('Wow, Now i understand http requests and responses')
+    // res.end()
+
+    // res.setHeader('Content-Type', 'text/html')
+    // res.write('<h2>Am i working?</h2>')
+    // res.end()
+
     res.setHeader('Content-Type', 'text/html')
-    res.write('<h2>Am i working?</h2>')
-    res.end()
+
+    // send an html file
+    fs.readFile('./views/index.html', (err, data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        }
+        res.write(data)
+        res.end()
+    })
 
 })
 server.listen(port, 'localhost', () => {
