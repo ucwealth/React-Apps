7266952af9d78166185f18c1f6d5346ffd906bfa
ucwealth <ucheokeke16@gmail.com>
2022-04-16 01:02:17 +0100

    restructure dirs

---
+++ node-app/core/server.js
@@ -0,0 +1,49 @@
+const http = require('http')
+const fs = require('fs')
+const port = 3000
+const _ = require('lodash')
+
+const server = http.createServer((req, res) => {
+
+    // lodash
+    const num = _.random(0, 20)
+    console.log(num)
+
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+
+    let path = '../views/'
+    switch(req.url) {
+        case '/':
+            path += 'index.html'
+            res.statusCode = 200
+            break
+        case '/about':
+            path += 'about.html'
+            res.statusCode = 200
+            break
+        case '/about-me': // Redirect from 'about-me' route to 'about' route   
+            res.statusCode = 301 // resource has been permanently moved
+            res.setHeader('Location', '/about')
+            res.end()
+            break
+        default:
+            path += '404.html'
+            res.statusCode = 404
+            break
+    }
+
+    // send an html file
+    fs.readFile(path, (err, data) => {
+        if(err){
+            console.log(err)
+            res.end()
+        }
+        // res.write(data)
+        res.end(data)
+    })
+
+})
+server.listen(port, 'localhost', () => {
+    console.log('listening on port ' + port)
+})
--- node-app/index.js
+++ node-app/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-    res.send('Hello from Naija')
-})
+// app.get('/', (req, res) => {
+//     res.send('Hello from Naija')
+// })
 
-app.listen(port, () => {
-    console.log('Listening on port ' + port)
-})
+// app.listen(port, () => {
+//     console.log('Listening on port ' + port)
+// })
